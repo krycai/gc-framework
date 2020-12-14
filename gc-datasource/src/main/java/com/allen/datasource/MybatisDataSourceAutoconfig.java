12300b98347b9eb783ff6e6df424d91cce30607d
xuguocai <xuguocai@bluemoon.com.cn>
2020-12-14 15:00:14 +0800

    数据源配置

---
+++ gc-datasource/src/main/java/com/allen/datasource/MybatisDataSourceAutoconfig.java
@@ -0,0 +1,10 @@
+package com.allen.datasource;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by xuguocai on 2020/12/14 14:00
+ */
+@ComponentScan("com.allen.datasource")
+public class MybatisDataSourceAutoconfig {
+}
---
+++ gc-datasource/src/main/java/com/allen/datasource/common/DynamicDataSourceGlobal.java
@@ -0,0 +1,15 @@
+package com.allen.datasource.common;
+
+/**
+ * Created by xuguocai on 2020/12/14 14:29
+ */
+public enum DynamicDataSourceGlobal {
+    /**
+     * 读数据源
+     */
+    READ,
+    /**
+     * 写数据源
+     */
+    WRITE
+}
