8de6ad07f9b7015b15df6d23e33c085d33a21dbb
xuguocai <xuguocai@bluemoon.com.cn>
2021-02-07 17:21:05 +0800

    数组

--- gc-thread/src/main/java/com/allen/thread/thread/CompletableFutureTest.java
+++ gc-thread/src/main/java/com/allen/thread/thread/CompletableFutureTest.java
@@ -3,6 +3,7 @@ package com.allen.thread.thread;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -31,4 +32,28 @@ public class CompletableFutureTest {
 
     }
 
+    @Test
+    public void test2(){
+        int[] a = new int[10];
+        a[0] = 0;
+        a[1] = 1;
+        a[2] = 2;
+        a[3] = 3;
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        System.out.println();
+
+        System.arraycopy(a, 1, a, 2, 4);
+        a[2]=99;
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    @Test
+    public void test3(){
+        List<String> list =new LinkedList<>();
+    }
 }
