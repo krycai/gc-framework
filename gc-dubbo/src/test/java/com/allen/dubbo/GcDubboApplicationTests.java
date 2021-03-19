e61bce1d0cb0e97945d75f5b8d28b2b7e4e97385
xuguocai <xuguocai@bluemoon.com.cn>
2021-03-19 14:13:44 +0800

    待学习项目

---
+++ gc-dubbo/src/test/java/com/allen/dubbo/GcDubboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allen.dubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcDubboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
