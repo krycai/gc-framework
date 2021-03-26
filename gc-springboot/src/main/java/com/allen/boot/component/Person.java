ccc5705b91c3bf1f7044a503e5aa85888682e454
xuguocai <xuguocai@bluemoon.com.cn>
2021-03-26 10:08:03 +0800

    spring 后置处理器

---
+++ gc-springboot/src/main/java/com/allen/boot/component/Person.java
@@ -0,0 +1,65 @@
+package com.allen.boot.component;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by xuguocai on 2021/3/26 8:53
+ *
+ * 加载顺序： BeanNameAware  --》BeanFactoryAware --》InitializingBean
+ *
+ */
+@Component
+public class Person implements InitializingBean, BeanFactoryAware, BeanNameAware, DisposableBean {
+
+    private String name;
+
+    private Integer age;
+
+    private Integer money;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getMoney() {
+        return money;
+    }
+
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactoryAware 的 setBeanFactory方法");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("BeanNameAware 的 setBeanName 的方法");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean 的 destroy 的方法");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitializingBean 的 afterPropertiesSet 的方法");
+    }
+}
