091b977f28bea389a0d06248f54e4df4758826c4
xuguocai <xuguocai@bluemoon.com.cn>
2021-04-29 17:30:11 +0800

    深拷贝

--- gc-design-pattern/src/main/java/com/allen/pattern/prototype/Prototype.java
+++ gc-design-pattern/src/main/java/com/allen/pattern/prototype/Prototype.java
@@ -2,6 +2,7 @@ package com.allen.pattern.prototype;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.*;
 import java.util.ArrayList;
 
 /**
@@ -53,16 +54,27 @@ public class Prototype implements Cloneable{
      * 将一个对象复制后，不论是基本数据类型还有引用类型，都是重新创建的。简单来说，就是深复制进行了完全彻底的复制，而浅复制不彻底。
      * @return
      */
-    public Prototype deepClone(){
-        Prototype deepCp = null;
-        try {
-            deepCp =(Prototype) super.clone();
-            deepCp.list = (ArrayList) this.list.clone();
-            log.info("执行深拷贝");
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return deepCp;
+    public Prototype deepClone()throws IOException, ClassNotFoundException {
+//        Prototype deepCp = null;
+//        try {
+//            deepCp =(Prototype) super.clone();
+//            deepCp.list = (ArrayList) this.list.clone();
+//            log.info("执行深拷贝");
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return deepCp;
+
+        /* 写入当前对象的二进制流 */
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(this);
+
+        /* 读出二进制流产生的新对象 */
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        return (Prototype)ois.readObject();
+
     }
 
 }
